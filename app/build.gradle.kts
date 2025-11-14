1478088d0d5d9050b9617548162ec85fe76a2aec
Rindra Hosea Mahefaniaina <mahefaniainarindra@gmail.com>
2025-11-14 09:07:18 +0300

    use firebase-boom and firebase-ai services

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,6 +51,8 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+    implementation(platform(libs.firebase.bom))
+    implementation(libs.firebase.ai)
     implementation(libs.firebase.auth)
     implementation(libs.androidx.compose.runtime.livedata)
     testImplementation(libs.junit)
